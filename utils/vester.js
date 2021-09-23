6889f4da60b5fd24b80d995eaf4105c3268b802b
Lewis Freiberg <1716917+l3wi@users.noreply.github.com>
2021-09-23 15:04:42 +0200

    Create helpers for token balances and tokens

--- utils/vester.js
+++ utils/vester.js
@@ -1,13 +1,65 @@
-import { ethers } from "ethers"
-import { web3, getSymbol } from "./ethers"
+import { ethers, ContractFactory } from "ethers"
+import { web3, getSymbol, fetchBalance } from "./ethers"
 import { Contract, Provider } from "ethers-multicall"
+import fetch from "isomorphic-fetch"
+import { Vester, VesterCode } from "../contracts"
 
-import { Vester } from "../contracts"
+export const verifyContract = async (address, constructor) => {
+  const response = await fetch(
+    "/api/verify?address=" + address + "&constructor=" + constructor
+  ).then((res) => res.json())
+  return response
+}
 
-export const getVesterInfo = async (address, token) => {
-  console.log("Args:", address, token)
-  const tokenSymbol = await getSymbol(token)
+export const getTokens = async (address) => {
+  const url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=999999999&sort=asc`
+  const response = await fetch(url).then((res) => res.json())
+  let tokens = []
+  try {
+    response.result.map((tx) => {
+      if (tokens.findIndex((item) => item.symbol === tx.tokenSymbol) === -1) {
+        tokens.push({ address: tx.contractAddress, symbol: tx.tokenSymbol })
+      }
+    })
+    return tokens
+  } catch (error) {
+    return []
+  }
+}
 
+export const mintVester = async (
+  beneficiary,
+  start,
+  cliffDuration,
+  duration,
+  revocable
+) => {
+  let iface = new ethers.utils.Interface(Vester.abi)
+  const constructor = iface.encodeDeploy([
+    beneficiary,
+    start,
+    cliffDuration,
+    duration,
+    revocable
+  ])
+
+  const signer = web3.getSigner()
+  const factory = new ContractFactory(Vester.abi, Vester.bytecode, signer)
+  const contract = await factory.deploy(
+    beneficiary,
+    start,
+    cliffDuration,
+    duration,
+    revocable
+  )
+  return {
+    address: contract.address,
+    transaction: contract.deployTransaction,
+    constructor
+  }
+}
+
+export const getVesterInfo = async (address) => {
   const ethcallProvider = new Provider(web3)
   await ethcallProvider.init()
   const vester = new Contract(address, Vester.abi)
@@ -18,23 +70,11 @@ export const getVesterInfo = async (address, token) => {
     vester.start(),
     vester.cliff(),
     vester.duration(),
-    vester.releaseableAmount(token),
-    vester.released(token),
-    vester.revocable(),
-    vester.revoked(token)
+    vester.revocable()
   ]
 
-  const [
-    owner,
-    beneficiary,
-    start,
-    cliff,
-    duration,
-    releaseableAmount,
-    released,
-    revocable,
-    revoked
-  ] = await ethcallProvider.all(calls)
+  const [owner, beneficiary, start, cliff, duration, revocable] =
+    await ethcallProvider.all(calls)
 
   return {
     owner,
@@ -42,11 +82,34 @@ export const getVesterInfo = async (address, token) => {
     start: start.toString(),
     cliff: cliff.toString(),
     duration: duration.toString(),
-    releaseableAmount: ethers.utils.formatEther(releaseableAmount),
-    released: ethers.utils.formatEther(released),
-    revocable,
-    revoked,
-    symbol: tokenSymbol
+    revocable
+  }
+}
+
+export const getVesterToken = async (address, token) => {
+  const balance = await fetchBalance(token.address, address)
+
+  const ethcallProvider = new Provider(web3)
+  await ethcallProvider.init()
+  const vester = new Contract(address, Vester.abi)
+
+  const calls = [
+    vester.releaseableAmount(token.address),
+    vester.released(token.address),
+    vester.revoked(token.address)
+  ]
+
+  const [releaseableAmount, released, revoked] = await ethcallProvider.all(
+    calls
+  )
+
+  return {
+    address: token.address,
+    symbol: token.symbol,
+    balance: parseFloat(balance),
+    releaseableAmount: parseFloat(ethers.utils.formatEther(releaseableAmount)),
+    released: parseFloat(ethers.utils.formatEther(released)),
+    revoked
   }
 }
 
